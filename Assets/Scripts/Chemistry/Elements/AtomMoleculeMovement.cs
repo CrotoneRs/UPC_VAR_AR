94f7d0a39ddbad7dbebf9618ad2f6b0328cdd323
AndMastro <andrea.mastropietro@gmail.com>
2018-12-03 19:11:06 +0100

    added some molecules labels

--- Assets/Scripts/Chemistry/Elements/AtomMoleculeMovement.cs
+++ Assets/Scripts/Chemistry/Elements/AtomMoleculeMovement.cs
@@ -20,7 +20,9 @@ namespace Assets.Scripts.Chemistry.Elements
             probField = new float[numElectrons];
             for (int i = 0; i < numElectrons; i++)
             {
-                probField[i] = Random.Range(1, numElectrons);
+                
+                probField[i] = Random.Range(0, numElectrons-1);
+                if (numElectrons == 1) probField[0] = 1;
             }
             
             
@@ -29,7 +31,7 @@ namespace Assets.Scripts.Chemistry.Elements
                 GameObject electron = this.transform.GetChild(i).gameObject;
 
                 /* Use Simplifyed Schrodinger Equation To Get Orbit */
-                float possiblePosition = probField[Random.Range(0, numElectrons - 1)] * Mathf.Exp(-40 * Time.deltaTime);
+                float possiblePosition = probField[Random.Range(0, numElectrons - 1)] * Mathf.Exp(-125 * Time.deltaTime);
                 electron.transform.RotateAround(this.transform.position, this.transform.TransformVector(new Vector3(possiblePosition , possiblePosition , possiblePosition )), 125 * Time.deltaTime);
             }
         }
