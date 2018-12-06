2af09c75f9dd7b36c41fe6ed92db4f3fe51a9193
AndMastro <andrea.mastropietro@gmail.com>
2018-12-06 21:23:11 +0100

    added Solar System implementation

---
+++ Assets/Scripts/Astronomy/MoonMovement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoonMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.Rotate(0, 0.5f, 0);
+        this.transform.RotateAround(this.transform.parent.transform.position, this.transform.parent.transform.TransformVector(new Vector3(0,1,0)), (0.5f/30) * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoaderScript : MonoBehaviour {
+
+	// Use this for initialization
+	 
+
+    public void LoadScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+}
